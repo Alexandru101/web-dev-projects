9de3848ea0877eb7ce6e3133b5288530b03642ef
Alexandru <alexandru.nic14@gmail.com>
2026-03-09 17:22:36 +0000

    Create index.js

---
+++ Real Time Chat Application/server/index.js
@@ -0,0 +1,31 @@
+// Importing Required Modules //
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+// Initializing Express and HTTP Server //
+const app = express();
+const server = http.createServer(app);
+
+// Setting Up Socket.IO //
+const io = socketIo(server);
+const date = new Date();
+let data = [];
+
+// Functionality //
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    socket.emit('server_message', `Local Host: http://localhost:9000\nDate: ${date.toLocaleString()}`);
+    socket.emit('server_data', data);
+
+    socket.on('send_message', (msg) => {
+        socket.broadcast.emit("send_message", msg);
+        data.push(msg);
+    });
+});
+
+server.listen(9000, () => {
+    console.log("Local Host: http://localhost:9000");
+    console.log(`${date.getDate().toString().padStart(2, "0")}/${date.getMonth().toString().padStart()}/${date.getFullYear()}`);
+});
